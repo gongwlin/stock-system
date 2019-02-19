77bf077da3026eb7c1a21e2725197b7ad78c6e9a
gongwlin <gongwlin@126.com>
2019-02-19 23:46:54 +0800

    feat: 支持session

--- bill-backend/index.js
+++ bill-backend/index.js
@@ -1,30 +1,30 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
-
+const session = require('koa-session');
 const app = new Koa();
 const router = new Router();
 const server = require('./server/connect');
 
 app.use(bodyParser());
 
-//  登入接口，调用login中间件处理
-// app.use('/login', bodyParser.json(),require('./router/login'));
+app.keys = ['some secret hurr'];
 
-// const login = ctx => {
-//     console.log('ctx',ctx.url);
-//     ctx.body = 'login';
-// }
+const CONFIG = {
+    key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+    /** (number || 'session') maxAge in ms (default is 1 days) */
+    /** 'session' will result in a cookie that expires when session/browser is closed */
+    /** Warning: If a session cookie is stolen, this cookie will never expire */
+    maxAge: 86400000,
+    autoCommit: true, /** (boolean) automatically commit headers (default true) */
+    overwrite: true, /** (boolean) can overwrite or not (default true) */
+    httpOnly: true, /** (boolean) httpOnly or not (default true) */
+    signed: true, /** (boolean) signed or not (default true) */
+    rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+    renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+};
 
-
-// app.post('/login', function (req, res) {
-//     console.log('req.body', req.body);
-//     const { usm, pwd } = req.body;
-//     console.log('router', usm, pwd);
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.send('aaaaa');
-// });
-
+app.use(session(CONFIG, app));
 
 router.post('/login', require('./router/login'));
 router.post('/register', require('./router/register'));
