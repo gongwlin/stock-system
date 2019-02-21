29df0306963373208359b72c797f2ba2c1d3aadf
gongwlin <gongwlin@126.com>
2019-02-21 18:50:22 +0800

    feat: demo

--- demo/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore,combineReducers } from 'redux';
-import reducer from './reducer';
-import reducer1 from './reducer1';
-
-const reducers = combineReducers({reducer,reducer1})
-const store = createStore(reducers);
-
-export default store;
