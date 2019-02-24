72cde2f3005d9b1e99f7a4382020c9e6ed4e2e2d
gongwlin <gongwlin@126.com>
2019-02-24 23:49:57 +0800

    feat: edit页面初步完成

--- demo/src/components/Tag/index.js
+++ demo/src/components/Tag/index.js
@@ -1,41 +1,17 @@
 import React from 'react';
+import './tag.css';
 
-export default class Icon extends React.PureComponent {
+export default class Tag extends React.PureComponent {
     
     render() {
-        const iconList = {
-            wx: 'icon-weixin', 
-            'close-eye': 'icon-eye1', 
-            eye: 'icon-eye', 
-            zfb: 'icon-umidd17', 
-            tongji: 'icon-tongji', 
-            jiechu: 'icon-jiechuchuku', 
-            ruku: 'icon-jieruruku', 
-            tag: 'icon-tag', 
-            gift: 'icon-liwu', 
-            house: 'icon-zhufang', 
-            rili: 'icon-rili', 
-            jiaotong:'icon-jiaotong', 
-            eat: 'icon-canyin', 
-            map: 'icon-ditu', 
-            share: 'icon-share', 
-            redpacket: 'icon-redpacket', 
-            game: 'icon-game', 
-            shopping: 'icon-goods', 
-            delete: 'icon-delete', 
-            setting: 'icon-settings', 
-            form: 'icon-form', 
-            more: 'icon-more', 
-            pay: 'icon-pay', 
-            taxi: 'icon-taxi', 
-            edit: 'icon-edit', 
-            close: 'icon-close'
-        }
+        const { style = {}, active, onClick = ()=>{} } = this.props
 
-        const { type, size = 20 } = this.props
-        if (!iconList[type]) return ''
         return (
-            <i className={`iconfont ${iconList[type]}`} style={{ fontSize: size}}></i>
+            <div className={ active ? ' bill-tag-active' : 'bill-tag'} 
+                onClick={() => onClick() }
+                style={style} >
+                {this.props.children}
+            </div>
         )
     }
 }
